edb756fa2fd1c28bca051666295dd03de154f19a
royalgamer12345 <royalgamer1551@gmail.com>
2021-07-16 00:06:51 +0530

    added basic react template
    first commit

---
+++ protofilo/src/MainApp/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <title>kek</title>
+  );
+}
+
+export default App;
---
+++ protofilo/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './MainApp/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
